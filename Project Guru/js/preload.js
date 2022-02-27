e5d2344469c71dbee8a11c1939d6795ebe91c87e
EphemeralXXV <74070371+EphemeralXXV@users.noreply.github.com>
2022-02-27 17:04:37 +0100

    Uploaded all the project files from my disk
    Contains the latest draft of the website that I've made, as well as all the resources that may be used for it, including variations of the Project Guru logo.

---
+++ Project Guru/js/preload.js
@@ -0,0 +1,25 @@
+/* Button animation */
+function turnButtonRed(button){
+    button.getElementsByClassName("leftArrow")[0].src = "../resources/navlefthover.png";
+    button.getElementsByClassName("rightArrow")[0].src = "../resources/navrighthover.png";
+    button.getElementsByTagName("span")[0].style.color = 'red';
+}
+
+function turnButtonWhite(button){
+    button.getElementsByClassName("leftArrow")[0].src = "../resources/navleft.png";
+    button.getElementsByClassName("rightArrow")[0].src = "../resources/navright.png";
+    button.getElementsByTagName("span")[0].style.color = 'yellow';
+}
+
+/* Navbar expansion */
+function expandNavbar() {
+    let navbarExpanded = document.getElementById("navbarExpanded");
+    let navbarExpandedStyle = window.getComputedStyle(navbarExpanded); // Get the #navbarExpanded CSS block
+    let maxHeight = navbarExpandedStyle.getPropertyValue('max-height'); // Read the value of max-height for #navbarExpanded
+    
+    maxHeight.replace(/\D/g, ''); // Gets rid of the unit (only the numeric value is left)
+    maxHeight = parseInt(maxHeight); // Conversion from string to integer
+    maxHeight = Math.abs(maxHeight - 100); // If maxHeight == 0, then maxHeight = |0 - 100| = 100; if maxHeight == 100, then maxHeight = |100 - 100| = 0
+    
+    navbarExpanded.style.maxHeight = `${maxHeight}%`;
+}
