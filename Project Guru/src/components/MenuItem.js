8fcf1821f0705e17c2d4ba57649acc60963c57ba
SpartaX18 <spartax18@gmail.com>
2022-09-05 21:02:06 +0200

    Refactor MenuItem button hover code to React-appropriate, get rid of obsolete preload.js

--- Project Guru/src/components/MenuItem.js
+++ Project Guru/src/components/MenuItem.js
@@ -1,25 +1,31 @@
 import { Link } from 'react-router-dom';
-import { turnButtonRed, turnButtonWhite } from '../common/preload';
+import { useState } from 'react';
 
 import navLeft from '../resources/navleft.png';
 import navRight from '../resources/navright.png';
+import navLeftHovered from '../resources/navlefthover.png';
+import navRightHovered from '../resources/navrighthover.png';
 
 import Styles from './MenuItem.module.css';
 
 const MenuItem = (props) => {
+    const [buttonHovered, setButtonHovered] = useState(false);
+    const handleButtonHover = () => {
+        setButtonHovered(prevState => !prevState);
+    }
     return (
         <div className = {Styles.menuItem} id = {Styles.menuForum}> {/*Container for individual menu item (icon + button)*/}
             <div className = {Styles.menuIcon} id = {Styles[`menu${props.name}Icon`]}> {/*Container for spinning menu icon*/}
                 <img src = {props.img} alt = {props.name} />
             </div>
             <div className = {Styles.menuButton} id = {Styles[`menu${props.name}Button`]}> {/*Container for menu button*/}
-                <div onMouseOver = {turnButtonRed(this)} onMouseOut = {turnButtonWhite(this)}>
+                <div onMouseOver = {handleButtonHover} onMouseOut = {handleButtonHover}>
                     <Link to = {`/${props.name.toLowerCase()}`}>
-                        <img className = {Styles.leftArrow} src = {navLeft} alt = 'Left arrow' />
-                        <span className = {Styles.buttonCenter} id = {Styles[`${props.name.toLowerCase()}Button`]}>
+                        <img className = {Styles.leftArrow} src = {buttonHovered ? navLeftHovered : navLeft} alt = 'Left arrow' />
+                        <span className = {Styles.buttonCenter} id = {Styles[`${props.name.toLowerCase()}Button`]} style = {buttonHovered ? {color: 'red'} : {color: 'yellow'}}>
                             {props.name}
                         </span>
-                        <img className = {Styles.rightArrow} src = {navRight} alt = 'Right arrow' />
+                        <img className = {Styles.rightArrow} src = {buttonHovered ? navRightHovered : navRight} alt = 'Right arrow' />
                     </Link>
                 </div>  
             </div>
