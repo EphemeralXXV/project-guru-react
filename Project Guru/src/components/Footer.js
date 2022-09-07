ba38fe5780172442c6e88013a74859e8edf0dbba
SpartaX18 <spartax18@gmail.com>
2022-09-07 22:15:27 +0200

    Update styles, add some tweaks

--- Project Guru/src/components/Footer.js
+++ Project Guru/src/components/Footer.js
@@ -2,7 +2,7 @@ import Styles from './Footer.module.scss';
 
 const Footer = () => {
     return (
-        <footer>
+        <footer id = {Styles.footer}>
             <p>https://ephemeralxxv.github.io/ Copyright <span style = {{fontFamily: 'sans-serif'}}>&copy;</span> Project Guru, 2022. All rights reserved.</p>
         </footer>
     );
