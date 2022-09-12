29c7d6192bc0df7f917a49be993b2e5c8267aa11
EphemeralXXV <ephemeralxxv@gmail.com>
2022-09-12 19:07:28 +0200

    Add some links to contests and community places

--- src/pages/Community.js
+++ src/pages/Community.js
@@ -2,6 +2,12 @@ const Community = () => {
     return (
         <>
             <p>Welcome to the Community page!</p>
+            <ul>
+                <li><a href = "https://rc2000.forumotion.com/">Forum</a></li>
+                <li><a href = "https://discord.gg/PAe3wHJ">Discord</a></li>
+                <li><a href = "https://rc2000.fandom.com/wiki/Rally_Championship_2000_Wiki">Wiki</a></li>
+                <li><a href = "https://redchili385.github.io/RC2K_JanuaryContest_2020/">Contest Website</a></li>
+            </ul>
         </>
     );
 }
--- src/pages/Contests.js
+++ src/pages/Contests.js
@@ -2,6 +2,40 @@ const Contests = () => {
     return (
         <>
             <p>Welcome to the Contests page!</p>
+            <br />
+            <ul>
+                <hr />
+                <li>2022:</li>
+                <ul>
+                    <li><a href = "https://redchili385.github.io/RC2K_JanuaryContest_2020/html/MFMI22/index.html">MFMI 2022</a></li>
+                </ul>
+                <hr />
+                <li>2021:</li>
+                <ul>
+                    <li><a href = "https://discord.com/channels/199990758089031680/419267523070918656/921356142687428619">The Frosty Chase</a></li>
+                    <li><a href = "https://discord.com/channels/199990758089031680/419267523070918656/983975697708113970">MFMI 2021</a></li>
+                    <li><a href = "https://rc2000.forumotion.com/t48-mobil-1-rally-championship-masters-2021">Mobil 1 Rally Championship Masters</a></li>
+                </ul>
+                <hr />
+                <li>2020:</li>
+                <ul>
+                    <li><a href = "https://redchili385.github.io/RC2K_JanuaryContest_2020/html/JulyContest/index.html">MFMI 2020</a></li>
+                    <li><a href = "https://redchili385.github.io/RC2K_JanuaryContest_2020/html/JanuaryContest/index.html">BRC: Milestone Rumble</a></li>
+                </ul>
+                <hr />
+                <li>2019:</li>
+                <ul>
+                    <li><a href = "https://docs.google.com/spreadsheets/d/1rYFKImlGIRS51B3_XdBPcQyaHsPjTBCRuE6zHhPGM4s">Mobil 1 Trailblazer</a></li>
+                    <li><a href = "https://rc2000.forumotion.com/t38-rally-championship-20th-anniversary-invitational-official-progress-thread">Rally Championship 20th Anniversary Invitational (MFMI 2019)</a></li>
+                    <li><a href = "https://discord.com/channels/199990758089031680/419267523070918656/573247979901681684">RallyMania 2019</a></li>
+                    <li><a href = "https://rc2000.forumotion.com/t35-mobil-1-tetrathlon">Mobil 1 Tetrathlon</a></li>
+                </ul>
+                <hr />
+                <li>2018:</li>
+                <ul>
+                    <li><a href = "https://docs.google.com/spreadsheets/d/1WLtr_eQwDEx2vuVmBKmAS6WYmD-4qSx1a0-LWfVW7oA">Magnetic Fields 30 Year Anniversary Memorial Invitational (MFMI 2018)</a></li>
+                </ul>
+            </ul>
         </>
     );
 }
