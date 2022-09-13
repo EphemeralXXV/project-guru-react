af0db79c3905d0a784d20e57fef1cf199b8cb376
EphemeralXXV <ephemeralxxv@gmail.com>
2022-09-13 23:25:27 +0200

    Add primitive collapsible component

--- src/pages/Misc.js
+++ src/pages/Misc.js
@@ -1,7 +1,15 @@
+import Collapsible from "@/components/Collapsible";
+
+import Favo from "@/resources/favo.gif"
+
 const Misc = () => {
     return (
         <>
             <p>Welcome to the Misc page!</p>
+            <Collapsible heading = "Hello world">
+                <img src = {Favo} />
+                <p>Ya like my car?</p>
+            </Collapsible>
         </>
     );
 }
