e36e8ddfee89b27dcaaabb846aeaf4c266858033
EphemeralXXV <ephemeralxxv@gmail.com>
2022-09-16 21:17:26 +0200

    Change the Navbar contents to something more appropriate, make it close upon clicking off

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,5 @@
 import { useState } from "react";
-
-import favo from "@/resources/favo.gif";
+import { Link } from "react-router-dom";
 
 import Styles from "./Navbar.module.scss";
 
@@ -8,14 +7,24 @@ const Navbar = () => {
     const [navbarOpen, setNavbarOpen] = useState(false);
     const handleNavbarToggle = () => {
         setNavbarOpen(prevState => !prevState);
+        document.addEventListener("click", (e) => closeNavbar(e));
+    }
+    const closeNavbar = (event) => {
+        if(!event.target.closest(`#${Styles.navbar}`)) {        // If #navbar isn't an ancestor of the clicked element... Template literal necessary because of the odd way CSS modules work
+            setNavbarOpen(false);
+            document.removeEventListener("click", closeNavbar); // Clean up
+        }
     }
     return (
-        <>
+        <div id = {Styles.navbar}>
             <div id = {Styles.navbarCollapsed} onClick = {handleNavbarToggle} />
             <div id = {Styles.navbarExpanded} style = {navbarOpen ? {maxHeight: "100%"} : {maxHeight: "0%"}}>
-                <img src = {favo} alt = "Favo" />
+                <p>Sign in</p>
+                <p>Username: <input type = "text" /></p>
+                <p>Password: <input type = "password" /></p>
+                <Link to = "/">Sign in</Link>
             </div>        
-        </>
+        </div>
     );
 }
 
