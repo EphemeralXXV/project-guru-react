ec368eaf444fde29e76d26a128dee17a2349901d
EphemeralXXV <ephemeralxxv@gmail.com>
2022-09-19 18:42:05 +0200

    Add metadata and some styling to the component, make first instance

--- src/components/DownloadsPreview.js
+++ src/components/DownloadsPreview.js
@@ -1,8 +1,21 @@
 import Styles from './DownloadsPreview.module.scss';
 
-const DownloadsPreview = () => {
+const DownloadsPreview = (props) => {
     return (
-        <>
-        </>
+        <div className = {Styles.downloadsPreview}>
+            <h1 className = {Styles.head}>
+                {props.name}
+            </h1>
+            <img className = {Styles.thumbnail} src = {props.thumbnail} alt = {props.name} />
+            <p className = {Styles.description}>
+                {props.description}
+            </p>
+            <hr />
+            <p>Author: {props.author}</p>
+            <p>Downloads: 999</p>
+            <p>Rating: 10/10</p>
+        </div>
     );
 }
+
+export default DownloadsPreview;
--- src/pages/Downloads.js
+++ src/pages/Downloads.js
@@ -1,7 +1,10 @@
+import DownloadsPreview from "@/components/DownloadsPreview";
+
 const Downloads = () => {
     return (
         <>
             <p>Welcome to the Downloads page!</p>
+            <DownloadsPreview name = "RC2KEdit" thumbnail = "https://cdn.discordapp.com/attachments/638834861796229120/662680465072717824/unknown.png" description = "Track editor" author = "Dummiesman" />
         </>
     );
 }
