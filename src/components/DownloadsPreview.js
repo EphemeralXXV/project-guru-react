9cbf854710df048da761cf5d34e34bc656dab285
EphemeralXXV <ephemeralxxv@gmail.com>
2022-09-20 20:58:02 +0200

    Polish the component's looks and functionality, add secondary font

--- src/components/DownloadsPreview.js
+++ src/components/DownloadsPreview.js
@@ -1,19 +1,56 @@
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+
+import downloadIcon from '@/resources/download_icon.png';
+
 import Styles from './DownloadsPreview.module.scss';
 
 const DownloadsPreview = (props) => {
+    const [thumbnailHovered, setThumbnailHovered] = useState(false);
+    const handleThumbnailHover = () => {
+        setThumbnailHovered(prevState => !prevState);
+    }
+    const abridgeDescription = (description) => {
+        description.trim();
+        if(description.length >= 100) {
+            description = description.substring(0, 98) + "...";
+        }
+        return description; 
+    }
+
     return (
         <div className = {Styles.downloadsPreview}>
             <h1 className = {Styles.head}>
                 {props.name}
             </h1>
-            <img className = {Styles.thumbnail} src = {props.thumbnail} alt = {props.name} />
-            <p className = {Styles.description}>
-                {props.description}
-            </p>
-            <hr />
-            <p>Author: {props.author}</p>
-            <p>Downloads: 999</p>
-            <p>Rating: 10/10</p>
+            <div className = {Styles.thumbnailDiv} onMouseOver = {handleThumbnailHover} onMouseOut = {handleThumbnailHover}>
+                <Link to = {"./" + props.name}>
+                    <img className = {Styles.thumbnailPic} src = {props.thumbnail} alt = {props.name} />
+                </Link>
+                <a href = "https://mega.nz/#!UlAiWKDa!PLThVqBPFf_gzXL-WvfcYIUNkMQPaoiVKcUNUsUsA10" target = "_blank" >
+                    <img className = {Styles.downloadIcon} src = {downloadIcon} style = {{visibility: thumbnailHovered ? "visible" : "hidden"}} alt = "Download" title = "Download" />
+                </a>
+            </div>
+            <div className = {Styles.content}>
+                <p className = {Styles.description}>
+                    {abridgeDescription(props.description)}
+                    <br />
+                    <Link to = {"./" + props.name}>Read more...</Link>
+                </p>
+                <hr style = {{margin: "8px 0"}}/>
+                <p>Category:&#0020; {/* &#0020; = whitespace */}
+                    <Link to = {"/category/" + props.category}>
+                        {props.category}
+                    </Link>
+                </p>
+                <p>Author:&#0020;
+                    <Link to = {"/user/" + props.author}>
+                        {props.author}
+                    </Link>
+                </p>
+                <p>Downloads: 999</p>
+                <p>Rating: 10/10</p>
+            </div>
         </div>
     );
 }
--- src/pages/Downloads.js
+++ src/pages/Downloads.js
@@ -1,11 +1,21 @@
+import Styles from "./Index.module.css";
+
 import DownloadsPreview from "@/components/DownloadsPreview";
 
 const Downloads = () => {
     return (
-        <>
-            <p>Welcome to the Downloads page!</p>
-            <DownloadsPreview name = "RC2KEdit" thumbnail = "https://cdn.discordapp.com/attachments/638834861796229120/662680465072717824/unknown.png" description = "Track editor" author = "Dummiesman" />
-        </>
+        <div id = {Styles.page}>
+            <div id = {Styles.main}>
+                <p>Welcome to the Downloads page!</p>
+                <DownloadsPreview
+                    name = "RC2KEdit"
+                    thumbnail = "https://cdn.discordapp.com/attachments/638834861796229120/662680465072717824/unknown.png"
+                    description = "Fully functional track editor for RC2K allowing terraforming, texture swapping, lighting modifications and so much more!"
+                    category = "Tools &amp; Utilies"
+                    author = "Dummiesman"
+                />
+            </div>
+        </div>
     );
 }
 
