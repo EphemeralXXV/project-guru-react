b80c8d2a00ab15830dd956d17891f1d212bf58a4
EphemeralXXV <ephemeralxxv@gmail.com>
2022-09-23 23:10:49 +0200

    Improve layout and styling, test on dummy data

--- src/components/ContestDetails.js
+++ src/components/ContestDetails.js
@@ -16,10 +16,18 @@ const ContestDetails = (props) => {
         <div className = {Styles.contestDetails}>
             <ul className = {Styles.tabs}>
                 {tabs.map((value, index) => {
-                    return <ContestDetailsTab name = {value} onTabClick = {handleTabChange} key = {index} active = {value === activeTab ? true : false}/>;
+                    return <ContestDetailsTab name = {value} onTabClick = {handleTabChange} key = {index} active = {value === activeTab ? true : false} />;
                 })}
             </ul>
-            <ContestDetailsPanel name = {activeTab}>
+            <ContestDetailsPanel
+                tab = {activeTab}
+                name = "MFMI21"
+                poster = "https://cdn.discordapp.com/attachments/419267523070918656/845239594152886292/flyer.png"
+                status = {{text: "Finished", color: "red"}} 
+                startDate = "29/07/2021"
+                endDate = "08/08/2021"
+                series = "MFMI"
+                host = "Ephemeral, Linotrix" >
                 {props[activeTab]}
             </ContestDetailsPanel>
         </div>
--- src/components/ContestDetailsPanel.js
+++ src/components/ContestDetailsPanel.js
@@ -1,9 +1,36 @@
+import { Link } from "react-router-dom";
+
 import Styles from "./ContestDetailsPanel.module.scss";
 
 const ContestDetailsPanel = (props) => {
     return (
         <div className = {Styles.contestDetailsPanel}>
-            {props.children}
+            <h1 className = {Styles.head}>
+                {props.name}
+            </h1>
+            <hr/>
+            <div className = {Styles.content}>
+                <div className = {Styles.details}>
+                    <img className = {Styles.poster} src = {props.poster} alt = {props.name} title = {props.name} />
+                    <div className = {Styles.info}>
+                        <p className = {Styles.status} style = {{color: props.status.color}}>{props.status.text}</p>
+                        <p>Start date: {props.startDate}</p>
+                        <p>End date: {props.endDate}</p>
+                        <p>Series:&#0020;
+                            {props.series === "none" ?
+                                "none" : 
+                                <Link to = {"/series/" + props.series}>
+                                    {props.series}
+                                </Link>
+                            }
+                        </p>
+                        <p>Hosted by: {props.host}</p>
+                    </div>
+                </div>
+                <p className = {Styles.description}>
+                    {props.children}
+                </p>
+            </div>
         </div>
     );
 }
