7112b51d56d01d760fb72b7f6305c2583abe4242
EphemeralXXV <ephemeralxxv@gmail.com>
2024-03-23 18:17:24 +0100

    Update footer copyright year.

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,7 @@ import Styles from "@/components/Footer.module.scss";
 const Footer: React.FC<{}> = () => {
     return (
         <footer id = {Styles.footer}>
-            <p>https://ephemeralxxv.github.io/ Copyright <span style = {{fontFamily: "sans-serif"}}>&copy;</span> Project Guru, 2022. All rights reserved.</p>
+            <p>https://ephemeralxxv.github.io/ Copyright <span style = {{fontFamily: "sans-serif"}}>&copy;</span> Project Guru, 2024. All rights reserved.</p>
         </footer>
     );
 }
