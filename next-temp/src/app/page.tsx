9add7f2f75c9e9e98f76629052e4fdf90a8cd5eb
EphemeralXXV <ephemeralxxv@gmail.com>
2025-06-21 20:54:41 +0200

    Fix font imports, remove Bootstrap and manually add back used styles (Bootstrap was only used on the Index page), add the favicon.

--- next-temp/src/app/page.tsx
+++ next-temp/src/app/page.tsx
@@ -1,5 +1,3 @@
-import Head from "next/head";
-
 import spinningComputers from "@/resources/spinningComputers.gif";
 import spinningArcade from "@/resources/spinningArcade.gif";
 import spinningFloppy from "@/resources/spinningFloppy.gif";
@@ -15,33 +13,7 @@ import Styles from "@/app/Index.module.scss";
 
 const IndexPage: React.FC<{}> = () => {
     return (
-        <>            
-            <Head>
-                <meta charSet = "UTF-8" />
-                <meta name = "viewport" content = "width = device-width, initial-scale = 1.0" />
-                <title>Index</title>
-                <script
-                    src = "https://code.jquery.com/jquery-3.4.1.slim.min.js"
-                    integrity = "sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n"
-                    crossOrigin = "anonymous"
-                />
-                <script
-                    src = "https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"
-                    integrity = "sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo"
-                    crossOrigin = "anonymous"
-                />
-                <script
-                    src = "https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"
-                    integrity = "sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6"
-                    crossOrigin = "anonymous"
-                />
-                <link
-                    rel = "stylesheet"
-                    href = "https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
-                    integrity = "sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh"
-                    crossOrigin = "anonymous"
-                />
-            </Head>
+        <>
             <div id = {Styles.page}>    
                 <Navbar />
                 <div id = {Styles.menuContainer}> {/*Container for the whole menu dashboard*/}
